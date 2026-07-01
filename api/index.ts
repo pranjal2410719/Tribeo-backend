c34569b1bf559ed116ce5e78b6037700c90c6ebf
Pranjal Yadav <2k24.cs1l.2410719@gmail.com>
2026-07-01 20:48:01 +0530

    Fix Vercel: use api/ directory

---
+++ api/index.ts
@@ -0,0 +1,22 @@
+import { buildServer } from '../src/app';
+
+let app: any;
+
+async function getApp() {
+  if (!app) {
+    app = await buildServer();
+    await app.ready();
+  }
+  return app;
+}
+
+export default async function handler(req: any, res: any) {
+  const server = await getApp();
+  server.server.emit('request', req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
